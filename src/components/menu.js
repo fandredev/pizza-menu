3bb4009a6955de80aa9637b4418c079c7e823934
felipe <profissionalf.andre@gmail.com>
2024-08-29 19:55:08 -0300

    chore: update menu component to include additional text and pizza options

--- src/components/menu.js
+++ src/components/menu.js
@@ -6,8 +6,15 @@ export default function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
+
       {hasPizzas ? (
-        <Pizza />
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <Pizza />
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later</p>
       )}
